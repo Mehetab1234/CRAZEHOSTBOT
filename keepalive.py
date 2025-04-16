23e0010bbfc4d5cd3c8c03aa2c21c1a0516f75e6
CRAFTINGCRAZEGAMIMG <mehetabhoque19@gmail.com>
2025-04-16 22:53:40 +0530

    Delete keepalive.py

--- keepalive.py
+++
@@ -1,9 +0,0 @@
-from keepalive import keep_alive
-import os
-
-# Function to run Flask app
-def run():
-    os.system("python app.py")  # Run the main Flask app
-
-# Keep the server alive
-keep_alive(run)
