9286a073add64d61a30b31885a1414e5dba02448
CRAFTINGCRAZEGAMIMG <mehetabhoque19@gmail.com>
2025-04-17 10:21:05 +0530

    Delete utils directory

--- utils/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from typing import Dict
-
-def load_config() -> Dict[str, str]:
-    """Load configuration from environment variables"""
-    return {
-        'token': os.getenv('DISCORD_TOKEN', ''),
-        'prefix': os.getenv('COMMAND_PREFIX', '!')
-    }
